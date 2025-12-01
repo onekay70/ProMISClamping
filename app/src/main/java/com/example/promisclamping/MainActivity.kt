a85e4a21f484dd0db844cb93c7b3661945b4306d
onekay70 <onekay70@gmail.com>
2025-12-01 16:33:30 +0800

    Update Screen size

--- app/src/main/java/com/example/promisclamping/MainActivity.kt
+++ app/src/main/java/com/example/promisclamping/MainActivity.kt
@@ -69,6 +69,8 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Upload
 import androidx.compose.material3.ButtonDefaults
@@ -212,6 +214,7 @@ fun DaftarKompaunScreen() {
             modifier = Modifier
                 .padding(padding)
                 .padding(16.dp)
+                .verticalScroll(rememberScrollState())
                 .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
--- app/src/main/java/com/example/promisclamping/presentation/kompaun/KompaunListContent.kt
+++ app/src/main/java/com/example/promisclamping/presentation/kompaun/KompaunListContent.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,7 +32,12 @@ fun KompaunListContent(
     canLoadMore: Boolean = false
 ) {
     Column(
-        modifier = modifier.padding(16.dp)
+        modifier = Modifier
+//            .padding(padding)
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+            .fillMaxSize(),
+//        modifier = modifier.padding(16.dp)
     ) {
         Text(
             text = title,
