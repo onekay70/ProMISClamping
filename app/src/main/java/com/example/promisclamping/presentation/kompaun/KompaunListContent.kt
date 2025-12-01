a709b4ab657e6ceaedc4c1c53cfa7c8f34d767c8
onekay70 <onekay70@gmail.com>
2025-12-01 17:58:19 +0800

    Update KompaunListContent.kt

--- app/src/main/java/com/example/promisclamping/presentation/kompaun/KompaunListContent.kt
+++ app/src/main/java/com/example/promisclamping/presentation/kompaun/KompaunListContent.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -32,12 +30,7 @@ fun KompaunListContent(
     canLoadMore: Boolean = false
 ) {
     Column(
-        modifier = Modifier
-//            .padding(padding)
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState())
-            .fillMaxSize(),
-//        modifier = modifier.padding(16.dp)
+        modifier = modifier.padding(16.dp)
     ) {
         Text(
             text = title,
